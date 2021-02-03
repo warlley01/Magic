0da2c2a0f6c3875eff72070cfffc596fef97d208
warlley01 <warlleyjhones@gmail.com>
2021-02-03 17:03:21 -0500

    update

--- Magic/javascript.js
+++ Magic/javascript.js
@@ -1,17 +1,79 @@
-var cube = document.getElementById('cube');
+var escolhidos = []
 
-var min = 1;
-var max = 24;
+window.onload = Myfunction()
+        
+function Myfunction() {
 
-cube.onclick = function() {
-  var xRand = getRandom(max, min);
-  var yRand = getRandom(max, min);
+    while(escolhidos.length < 86){
+        play = Math.floor(Math.random() * 86 + 1);  
+
+        if(escolhidos.indexOf(play) == -1){
+            escolhidos.push(play)
+        }
+
+        
+
+    }
+
+    console.log(escolhidos)
+    console.log(escolhidos[0])
     
-  cube.style.webkitTransform = 'rotateX('+xRand+'deg) rotateY('+yRand+'deg)';
-  cube.style.transform = 'rotateX('+xRand+'deg) rotateY('+yRand+'deg)';
 }
 
-function getRandom(max, min) {
-  return (Math.floor(Math.random() * (max-min)) + min) * 90;
+
+function planos(){
+        play = Math.floor(Math.random() * 86 + 1);
+        document.getElementById("card").src = "./planos/"+escolhidos[0]+".jpg";  
+
+        escolhidos.splice(0, 1)
+        console.log(escolhidos)
+        console.log(escolhidos[0])
+
+    }
+
+function effect() {
+    var h = window.document.getElementById("dado")
+    h.style.background = 'green';     
+}
+function effect1(){
+    var h = window.document.getElementById("dado")
+    h.style.background = "black"
+}
+        function myFunction(){
+        var play = Math.floor(Math.random() * 6 + 1);
+        switch (play) {
+            case 1:                        
+                document.getElementById("lado").src = "./imgdado/dado1.jpg"
+                break            
+            case 2:
+                document.getElementById("lado").src = "./imgdado/dado2.jpg"
+                break
+            case 3:
+                document.getElementById("lado").src = "./imgdado/dado3.jpg"
+                break
+            case 4:
+                document.getElementById("lado").src = "./imgdado/dado4.jpg"
+                break
+            case 5:
+                document.getElementById("lado").src = "./imgdado/dado5.png"
+                break
+            case 6:
+                document.getElementById("lado").src = "./imgdado/dado6.jpg"
+    }
+} 
+
+
+
+function functionProximo(){
+    
+    
 }
 
+
+
+
+
+
+    
+
+   
